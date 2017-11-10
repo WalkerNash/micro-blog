3fe87f121c47e460092ec8469faa65ee65929c89
Fatima Burke <fatima@lurmarketing.com>
2017-11-10 12:56:54 -0500

    initial commit

--- app.rb
+++ app.rb
@@ -11,12 +11,16 @@ get '/' do
   erb :index
 end
 
-# Providing model information to the view
-# requires an instance variable (prefixing with the '@' symbol)
+# Fatima's routes
+get '/logout' do
+  session.clear
+  redirect '/'
+end
 
-# Example 'User' index route
-
-# get '/users' do
-#   @users = User.all
-#   erb :users
-# end
+post '/login' do
+  username = params[:username].downcase
+  user = User.find_or_create_by(username: username)
+  session[:user_id] = user.id
+  redirect "/"
+end
+# End Fatima's routes

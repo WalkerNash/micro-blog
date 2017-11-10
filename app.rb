5a2f955d24983b2c77210d1477246a1a97461e36
michaelJohnJames <32465022+michaelJohnJames@users.noreply.github.com>
2017-11-10 12:56:13 -0500

    add account view and routes

--- app.rb
+++ app.rb
@@ -11,12 +11,217 @@ get '/' do
   erb :index
 end
 
-# Providing model information to the view
-# requires an instance variable (prefixing with the '@' symbol)
 
-# Example 'User' index route
 
-# get '/users' do
-#   @users = User.all
-#   erb :users
-# end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Mike's routes start here
+
+get '/account' do
+  if current_user
+    erb :account
+  else
+    redirect '/'
+  end
+end
+
+patch '/profile/:id' do
+  user = User.find_by_id(params[:id])
+  user.update(
+    username: params[:username],
+    f_name: params[:f_name],
+    l_name: params[:l_name],
+    location: params[:location],
+    email: params[:email]
+  )
+  redirect "/profile/#{user.id}"
+end
+
+
+delete '/profile/:id' do
+  user = User.find_by_id(params[:id])
+  user.destroy
+  redirect '/'
+end
